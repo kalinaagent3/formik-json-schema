426bfb3618cc342cb8915e1d3938aa90bd91c47c
kalinagercheva3 <kalina.gercheva@agent3.com>
2022-07-27 15:59:58 +0300

    fixes

--- demo/src/schema/all-available-fields.js
+++ demo/src/schema/all-available-fields.js
@@ -81,7 +81,7 @@ export default {
         as: "p",
         htmlClass: "text-muted d-block mb-3 mt-1",
         defaultValue:
-          "This is some raw html text content: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+          "<b>This is some raw html text content: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </b>",
       },
       autocomplete: {
         name: "autocomplete",
--- src/Field/InnerText.js
+++ src/Field/InnerText.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const InnerText = ({ config, formik, value, error }) => {
-    const {
-        name,
-        as: Component = 'span',
-        htmlClass,
-        defaultValue = '',
-        ...attributes
-    } = config;
+  const {
+    name,
+    as: Component = "span",
+    htmlClass,
+    defaultValue = "",
+    ...attributes
+  } = config;
 
-    return (
-        <Component className={ htmlClass } { ...attributes }>
-            { value || defaultValue }
-        </Component>
-    );
+  return (
+    <Component className={htmlClass} {...attributes}>
+      {<span dangerouslySetInnerHTML={{ __html: value || defaultValue }} />}
+    </Component>
+  );
 };
 
 export default React.memo(InnerText);
